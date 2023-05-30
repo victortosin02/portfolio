4e0206431194e8cb7a8a4f36ab7ed5242a7a46e3
victortosin02 <victor.oladejo@cloudware.ng>
2023-05-30 09:12:03 +0100

    new one

--- pages/index.js
+++ pages/index.js
@@ -10,8 +10,8 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Clint | Front-End Developer</title>
-        <meta name="description" content="I’m a front-end web developer specializing in building (and occasionally designing) exceptional digital experiences." />
+        <title>Agboola Oyekola | Product Designer</title>
+        <meta name="description" content="I’m a product designer specialized in designing user interfaces for an exceptional digital experiences." />
         <link rel="icon" href="/fav.png" />
       </Head>
     <Main />
